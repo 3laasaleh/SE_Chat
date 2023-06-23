518ae82cd18d0d6102830c401c4e92a8eed850c4
Yasser Hosny <yasser_hosny@siliconexpert.com>
2023-06-23 16:02:08 +0300

    More enhance for app interface

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -12,24 +12,34 @@ interface ChatMessage {
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-public messages: ChatMessage[] = [];
- public userMessage: string = '';
- apiUrl='api ur';
-  constructor(private http: HttpClient) {}
+  public messages: ChatMessage[] = [];
+  public userMessage: string = '';
+  apiUrl = 'http://localhost:5000/tapas';
+  constructor(private http: HttpClient) { }
 
   sendMessage() {
     const userText = this.userMessage.trim();
+    console.log(userText);
     if (userText) {
       this.messages.push({ text: userText, sender: 'user' });
-      this.userMessage ='' ;
-      this.messages.push({ text: 'sorry cann`t find answer', sender: 'bot' });
-      // Send user message to ChatGPT API
-      this.http.post<any>(this.apiUrl, { message: userText })
+      // this.userMessage = '';
+      // this.messages.push({ text: 'sorry can`t find answer', sender: 'bot' });
+      // Send user message to Chat-bot API
+      this.http.post<any>(this.apiUrl, { user_input: userText })
         .subscribe(response => {
-          const botText = response.message ==null  ? 'sorry cann`t find answer ' :response.message ;
-        
+          console.log(response);
+          const botText = response.bot_response == null ? 'sorry can`t find answer ' : response.bot_response.answer;
+          console.log(response.bot_response);
+
           this.messages.push({ text: botText, sender: 'bot' });
+          this.userMessage = "";
         });
     }
   }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }  
 }
